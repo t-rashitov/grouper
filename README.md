6cabb5bc18a88a3cbb5a8ab2c3e6afde54635288
rashitov.t <devourndevide@gmail.com>
2021-10-28 15:43:07 +0300

    added README.md, added docstrigs to views, added pydantic validation to grouping view

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # grouper
-Service for news texts grouping
+
+**_Сервис группировки похожих текстов_**
+
+**Для запуска приложения в docker контейнере:**
+
+`docker-compose -f _CI/docker-compose.yml build`
+
+`docker-compose -f _CI/docker-compose.yml up`
+
+**Для запуска приложения локально:**
+
+_1. установить зависимости_
+
+    pip install -r requirements.txt
+
+_2. запустить сервер redis_
+
+    redis-server
+
+_1. запустить проект_
+
+    uvicorn app.grouper.main:application --host 0.0.0.0 --port 8080 --workers 4
+
+**Для запуска unit-тестов:**
+
+_1. установить pytest_
+    
+    pip install pytest
+
+_2. запустить прохождение тестов_
+
+    pytest
+
+
+**URL:**
+
+    /docs - документация API
+
+
--- app/grouper/views.py
+++ app/grouper/views.py
@@ -1,9 +1,7 @@
 import logging
-from json import JSONDecodeError
 
 from fastapi import APIRouter, HTTPException
 from fastapi.requests import Request
-from pydantic import ValidationError
 from redis import RedisError
 
 from grouper.models import ArticlesInRequest
@@ -16,32 +14,25 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
-@router.post('/', name='add-to-groups', status_code=201)
-async def add_to_groups(request: Request) -> dict:
-    try:
-        json_data = await request.json()
-    except JSONDecodeError:
-        raise HTTPException(status_code=400, detail='Must provide JSON data in request.')
+@router.post('/', name='add-to-groups', status_code=200)
+async def add_to_groups(articles_data: ArticlesInRequest) -> dict:
+    """Группировка текстов по группам"""
 
-    try:
-        articles = ArticlesInRequest(articles=json_data.get('articles'))
-    except ValidationError as e:
-        msg = str(e)
-        logger.debug(msg)
-        raise HTTPException(status_code=422, detail=msg)
-
-    groups = await get_groups(articles.articles)
+    groups = await get_groups(articles_data.articles)
 
     return {'groups': groups}
 
 
 @router.post('/clean-db', name='clean-db')
 async def clean_db(request: Request) -> dict:
+    """Очистка хранилища (redis) хешей (minhash)"""
+
     try:
         request.app.state.storage.flushall()
     except RedisError as e:
         msg = str(e)
         logger.debug(msg)
+
         raise HTTPException(status_code=500, detail=msg)
 
     return {'status': 'success'}
